9ed5647aac05720fff7203de0906a6bf356b5b7e
Chintan Udani <chintanudani2000@gmail.com>
2023-08-26 19:41:47 +0530

    updated api route

---
+++ src/api/api.js
@@ -0,0 +1,14 @@
+// api.js
+
+import axios from 'axios';
+
+// Create an instance of Axios with default configurations
+const instance = axios.create({
+  baseURL: 'https://your-api-base-url.com', // Replace with your API base URL
+  timeout: 10000, // Set a timeout for requests (optional)
+  headers: {
+    'Content-Type': 'application/json', // You can set default headers here
+  },
+});
+
+export default instance;
