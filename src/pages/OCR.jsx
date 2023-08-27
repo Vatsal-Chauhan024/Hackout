ad019325b594b1bdbf9c82fbd095840db8827db4
Vatsal-Chauhan024 <adarsh_vc24@yahoo.com>
2023-08-27 09:02:16 +0530

    OCR updated

--- src/pages/OCR.jsx
+++ src/pages/OCR.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from '../api/api'
 import useAuth from '../helper/useAuth';
+import { ToastContainer, toast } from 'react-toastify';
 
 const OCR = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -34,7 +35,7 @@ const OCR = () => {
         })
         
         .catch(error => {
-          console.error('Error fetching data:', error);
+          toast.error(error)
         })
         .finally(() => {
           setIsLoading(false);
@@ -45,6 +46,7 @@ const OCR = () => {
 
   return (
     <div>
+        <ToastContainer/>
       <div className='w-full px-10 mt-7'>
         <div className="alert alert-info rounded-md h-24 flex items-center">
           <span className='sm:text-2xl font-semibold text-lg'>Image To Text Conversion & Speech To Text Conversion: </span>
@@ -84,7 +86,7 @@ const OCR = () => {
   >
     <textarea
       id="OrderNotes"
-      className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm p-2 min-h-[100px]"
+      className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm p-2 min-h-[100px] outline-none"
       rows="4"
       placeholder="Enter any additional order notes..."
       value={extractedContent}
@@ -121,7 +123,7 @@ const OCR = () => {
   >
     <textarea
       id="OrderNotes"
-      className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm p-2 min-h-[100px]"
+      className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm p-2 min-h-[100px] outline-none"
       rows="4"
       placeholder="Enter any additional order notes..."
       
