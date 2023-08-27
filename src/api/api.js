d482d14f4658944871d00db1ada63457244be38d
Vatsal-Chauhan024 <adarsh_vc24@yahoo.com>
2023-08-27 09:13:12 +0530

    api wrapped

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of Axios with default configurations
 const instance = axios.create({
-  baseURL: 'https://hackout23-xvte.onrender.com', // Replace with your API base URL
+  baseURL: 'http://localhost:8000', // Replace with your API base URL
   timeout: 10000, // Set a timeout for requests (optional)
   headers: {
     'Content-Type': 'application/json', // You can set default headers here
